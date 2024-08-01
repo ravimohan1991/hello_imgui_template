dae2ab24f012af1f5887ce19d4d0db5de82f6330
BellInequality <ravimohan1991@gmail.com>
2024-08-02 04:17:09 +0530

    Minor adjustments for our purpose

--- hello_world.main.cpp
+++ hello_world.main.cpp
@@ -2,13 +2,21 @@
 
 int main(int , char *[])
 {
-    auto guiFunction = []() {
-        ImGui::Text("Hello, ");                    // Display a simple label
-        HelloImGui::ImageFromAsset("world.jpg");   // Display a static image
-        if (ImGui::Button("Bye!"))                 // Display a button
-            // and immediately handle its action if it is clicked!
-            HelloImGui::GetRunnerParams()->appShallExit = true;
-     };
-    HelloImGui::Run(guiFunction, "Hello, globe", true);
-    return 0;
+	auto guiFunction = []()
+	{
+		ImGui::Text("Hello, ");                    // Display a simple label
+		
+		static bool show = true;
+		ImGui::ShowDemoWindow(&show);
+		
+		HelloImGui::ImageFromAsset("world.jpg");   // Display a static image
+		if (ImGui::Button("Bye!"))                 // Display a button
+		{
+			// and immediately handle its action if it is clicked!
+			HelloImGui::GetRunnerParams()->appShallExit = true;
+		}
+	};
+	
+	HelloImGui::Run(guiFunction, "Hello, globe", true);
+	return 0;
 }
